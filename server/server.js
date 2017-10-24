af6e7174fd10608d6fead6430336ec892d84b4ad
michal <gelhrid@gmail.com>
2017-10-24 19:44:20 +0200

    dsf

--- server/server.js
+++ server/server.js
@@ -16,10 +16,31 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected');
+  // socket.emit('newEmail', {
+  //   from: "myszka@gmail.com",
+  //   text: "tresc maila",
+  //   createdAt: 213
+  // });
+
+  socket.emit('newMessage', {
+    from: "myszka@gmail.com",
+    text: "tresc maila",
+    createdAt: 213
+  });
+
+  // socket.on('createEmail', (newEmailData) => {
+  //   console.log('create Email', newEmailData);
+  // });
+
+
+  socket.on('createMessage', (createMessage) => {
+    console.log('new createMessage', createMessage);
+  });
   //to w tym poprzednim jest bo inaczej by nasluchiwalo pewnie na wsyztkie a tak na konkretny ten jeden
   socket.on('disconnect', (socket) => {
     console.log('client  dissconected');
   });
+
 });
 //poza blokiem nie dziala!
 // io.on('disconnect', (socket) => {
